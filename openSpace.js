4c36128e203bb4a8ef654f7d842b1a4a83553d6d
jakubgatarski <jakubgat1@gmail.com>
2021-12-15 23:14:56 +0100

    5th stage NOT completed, rocket fly at launch button but this button is not blocked by other functions

--- openSpace.js
+++ openSpace.js
@@ -2,7 +2,9 @@ let unlockBtn = document.getElementById("unlock-btn");
 let password = document.getElementById("password");
 let checks = document.querySelectorAll(".check");
 let ranges = document.querySelectorAll(".range");
-let launchBtn = document.getElementById("launch-btn")
+let launchBtn = document.getElementById("launch-btn");
+let rocket = document.querySelector(".rocket");
+
 function checkPassword(inputPass){
     if (inputPass == 'TrustNo1'){
         return true;
@@ -21,7 +23,7 @@ unlockBtn.addEventListener("click", function (){
         for (let j=0; j < ranges.length; j++){
             ranges[j].disabled = false;
         }
-        launchBtn.disabled = false;
+        //launchBtn.disabled = false;
         unlockBtn.disabled = true;
         password.disabled = true;
     }
@@ -30,3 +32,38 @@ unlockBtn.addEventListener("click", function (){
     }
 });
 
+
+launchBtn.addEventListener("click", function (){
+//setInterval(moveRocket,10);
+    //let position = rocket.getBoundingClientRect();
+    let rocketTop = 250//position.top + Math.sin(30*3.14/180)*75;
+    let rocketLeft = 520//position.left + Math.sin(30*3.14/180)*75;
+
+    setInterval(moveRocket,10);
+
+    function moveRocket(){
+        rocketTop --;
+        rocketLeft ++;
+        rocket.style.top = rocketTop + 'px';
+        rocket.style.left = rocketLeft + 'px';
+    }
+});
+
+/*function getRocketPosition(){
+    let position = rocket.getBoundingClientRect();
+    let rocketTop = position.top;
+    let rocketLeft = position.left;
+}*/
+
+/*
+function moveRocket(){
+    let position = rocket.getBoundingClientRect();
+    let rocketTop = position.top;
+    let rocketLeft = position.left;
+
+    rocketTop ++;
+    rocketLeft ++;
+    rocket.style.top = rocketTop + 'px';
+    rocket.style.left = rocketLeft + 'px';
+
+}*/

16bc5819d3a6415bd631b005300a19432335fb00
jakubgatarski <jakubgat1@gmail.com>
2021-12-20 19:35:42 +0100

    now you can control rocket speed and angle after correct password and good set of other params

--- openSpace.js
+++ openSpace.js
@@ -4,6 +4,8 @@ let checks = document.querySelectorAll(".check");
 let ranges = document.querySelectorAll(".range");
 let launchBtn = document.getElementById("launch-btn");
 let rocket = document.querySelector(".rocket");
+let speedBtn = document.getElementById("speed-change");
+let angleBtn = document.getElementById("angle-change");
 
 function checkPassword(inputPass){
     //checking is value the same as password;
@@ -29,6 +31,7 @@ unlockBtn.addEventListener("click", function (){
         //disables input password and unlock btn
         unlockBtn.disabled = true;
         password.disabled = true;
+
     }
     else {
         //if wrong password display alert
@@ -40,14 +43,16 @@ unlockBtn.addEventListener("click", function (){
 launchBtn.addEventListener("click", function (){
     //take rocket actual position
     //let position = rocket.getBoundingClientRect();  //To do get exact position of rocket by some func
-    let rocketTop = 250 //position.top + Math.sin(30*3.14/180)*75;
-    let rocketLeft = 520//position.left + Math.sin(30*3.14/180)*75;
+    let rocketTop = Number(getComputedStyle(rocket).top.substr(0, getComputedStyle(rocket).top.length - 2));  //position.top //+ Math.sin(30*3.14/180)*75; 250
+    let rocketLeft = Number(getComputedStyle(rocket).left.substr(0, getComputedStyle(rocket).left.length - 2));  //position.left + Math.sin(angle*3.14/180)*150;// 520
     //animates rocket, by changing its position values every 10ms
     setInterval(moveRocket,10);
     //function to change rocket top and left position
     function moveRocket(){
-        rocketTop --;
-        rocketLeft ++;
+        let angle = document.getElementById("angle-change").value;
+        let speed = document.getElementById("speed-change").value;
+        rocketTop = rocketTop - Math.cos(angle*3.14/180)*speed;
+        rocketLeft = rocketLeft + Math.sin(angle*3.14/180)*speed;
         rocket.style.top = rocketTop + 'px';
         rocket.style.left = rocketLeft + 'px';
     }
@@ -61,8 +66,20 @@ function checkInputs(){
         x += parseInt(ranges[j].value);
     }
     //if sum of slider = 500(maximum) and all checkbox are checked enables launch button
-    if(document.querySelectorAll('input[type="checkbox"]:checked').length === document.querySelectorAll('input[type="checkbox"]').length && x == 500) {
+    if(document.querySelectorAll('input[type="checkbox"]:checked').length === document.querySelectorAll('input[type="checkbox"]').length
+        && x == 500) {
+        //enable to start and change speed and angle of rocket
         launchBtn.disabled = false;
-
+        angleBtn.disabled = false;
+        speedBtn.disabled = false;
     }
 }
+
+function changeAngle(){
+    //get actual angle value
+    let angle = document.getElementById("angle-change").value;
+    let style = rocket.style;
+    //change angle in css properties
+    style.transform = 'rotate('+angle+'deg)';
+}
+
